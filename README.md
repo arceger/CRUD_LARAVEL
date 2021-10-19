4b94cf8e44a12821f79fe329377cd10cac8734aa
Gerson Dos Santos Arce <arceger@br.ibm.com>
2021-10-19 11:08:02 -0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+## TESTES COM LARAVEL CRUD
+
+
+
+- Conexao com banco local mysql 
+- usando modelo MVC 
+
+## init
+
+- php artisan serve
+
+## acess
+http://127.0.0.1:8001/contacts/
---
+++ resources/views/base1.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+    <title>HTML CRUD</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+    @yield('main')
+    </div>
+</body>
+</html>
